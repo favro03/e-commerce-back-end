6d2ef503a0b25aec7a0ad794e0b15363520c92b1
tfavro <wetr9902@gmail.com>
2022-02-22 18:13:23 -0600

    updated product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -70,7 +70,13 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+    Product.create({
+      product_name: req.body.product_name,
+      price: req.body.price,
+      stock: req.body.stock,
+      category_id: req.body.category_id,
+      tagIds: req.body.tagIds
+    })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -133,6 +139,7 @@ router.put('/:id', (req, res) => {
       res.status(400).json(err);
     });
 });
+
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
